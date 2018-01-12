bfabcf2c7f6010f3fdd9200ce76be305b95ec9bb
952334gh <khramovw@gmail.com>
2018-01-12 13:22:46 +0200

    img resize function

--- js/plugin.js
+++ js/plugin.js
@@ -179,16 +179,12 @@ function footerMobile(){
 }
 
 function resizeImg() {
-    var xxx = $('.resume-foto').width() - $('.resume-3').width();
     if( $('.resume-img').width() < $('.resume-foto').width()){
         $('.resume-img').css({
             'left'      : '50%',
             'transform' : 'translateX(-50%) scale(1.211)',
             'position'  : 'absolute'
-
         });
-
-        console.log('xxx', $('.resume-3').width(), $('.resume-foto').width(), xxx);
     }
 }
 
