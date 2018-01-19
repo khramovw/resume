0a1bdb6df9d73325e2f012457fd75789c1a7aea7
khramovw <khramovw@gmai.com>
2018-01-19 17:57:01 +0200

    cropbox-box upd

--- js/cropbox_img.js
+++ js/cropbox_img.js
@@ -1,13 +1,15 @@
-
+//нужно добавить клас .cropbox-box к .cropbox-img  = <div class="similar-box cropbox-box">
+// и добавить класс на изоброжение с авито .cropbox-img
 
 function cropboxIimg() {
 
     $('.cropbox-box a').each(function(index,elem){
         var findImg = $(this).find('.cropbox-img');
         var findImgSrc = $(this).find('.cropbox-img').attr('src'),
-            newDiv = `<div class="newdiv" style="overflow: hidden; width: 137.5px; height: 137.5px;"><img class="cropbox-img" src="${findImgSrc}" alt=""></div>`;
+            newDiv = `<div class="newdiv" style="overflow: hidden; "><img class="cropbox-img" src="${findImgSrc}" alt=""></div>`;
         $(this).find('.cropbox-img').remove();
         $(this).prepend(newDiv);
+        $('.newdiv').height($('.newdiv').width());
         console.log(findImg);
     });
     console.log('cropbox');
